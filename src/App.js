b39c2c0e55961d9e75b332944616581822acd274
Anthony Gaff <anthony.gaff@valid.com>
2020-01-14 09:49:01 -0500

    first push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Carrots or Garbage lives here.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
